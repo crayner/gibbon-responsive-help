2cb37857b682d8b0ee7b6b9ea8ba52c46234a9ce
Craig Rayner <craig@craigrayner.com>
2018-12-17 17:27:14 +1100

    Add CKEditor

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
